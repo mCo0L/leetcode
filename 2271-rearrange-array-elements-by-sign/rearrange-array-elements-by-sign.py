289849dd523e54213350fbd842cfafb6be76e3a5
Mukul Chauhan <12373994+mCo0L@users.noreply.github.com>
2024-07-10 16:55:21 +0100

    Time: 1022 ms (37.86%) | Memory: 50.9 MB (5.21%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -2,22 +2,12 @@ class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         positiveNums = list()
         negativeNums = list()
-        newArr = list()
-        for num in nums:
+
+        # Separate positive and negative numbers
+        for num in nums: 
             if num < 0:
                 negativeNums.append(num)
             else:
                 positiveNums.append(num)
-        
-        positiveIndex = 0
-        negativeIndex = 0
 
-        for i in range(len(nums)):
-            if i%2==0:
-                newArr.append(positiveNums[positiveIndex])
-                positiveIndex += 1
-            else:
-                newArr.append(negativeNums[negativeIndex])
-                negativeIndex += 1
-
-        return newArr
+        return itertools.chain(*zip(positiveNums, negativeNums))
