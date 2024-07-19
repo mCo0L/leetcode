c3749c1c598bf8b8f5bf7829d7924205acd01855
Mukul Chauhan <12373994+mCo0L@users.noreply.github.com>
2024-07-19 21:53:51 +0100

    Time: 110 ms (61.21%) | Memory: 16.9 MB (15.82%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
@@ -1,22 +1,15 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        
-        # Step 1: Find the minimum element in each row
         min_row_values = [min(row) for row in matrix]
-        
-        # Step 2: Identify the columns of the minimum elements
         min_row_positions = {i: row.index(min(row)) for i, row in enumerate(matrix)}
-        
-        # Step 3: Find the maximum element in each column
         max_col_values = [max(col) for col in zip(*matrix)]
-        
-        lucky_numbers = []
-        
-        # Step 4: Check if the minimum element in the row is the maximum in its column
+
+        lucky_numbers = list()
+
         for i, min_value in enumerate(min_row_values):
             column_index = min_row_positions[i]
             if min_value == max_col_values[column_index]:
                 lucky_numbers.append(min_value)
-        
+    
         return lucky_numbers
                 
