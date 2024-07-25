9249d39f903b2ef76699a27968853d62315edb82
Mukul Chauhan <12373994+mCo0L@users.noreply.github.com>
2024-07-25 22:55:43 +0100

    Time: 1117 ms (55.03%) | Memory: 24 MB (77.47%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,15 +1,44 @@
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        counting: list[int] = [0 for _ in range(2 * 5 * 10**4 +1)]
+    def mergeSort(self, myList):
+        if len(myList) > 1:
+            mid = len(myList) // 2
+            left = myList[:mid]
+            right = myList[mid:]
 
-        for num in nums:
-            counting[num + 5 * 10**4 ] += 1
-        
-        write_ind: int = 0
-        for number_ind, freq in enumerate(counting):
-            while freq != 0:
-                nums[write_ind] = number_ind - 5 * 10**4
-                write_ind += 1
-                freq -= 1
-        
+            # Recursive call on each half
+            self.mergeSort(left)
+            self.mergeSort(right)
+
+            # Two iterators for traversing the two halves
+            i = 0
+            j = 0
+            
+            # Iterator for the main list
+            k = 0
+            
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                
+                    myList[k] = left[i]
+                
+                    i += 1
+                else:
+                    myList[k] = right[j]
+                    j += 1
+            
+                k += 1
+
+            # For all the remaining values
+            while i < len(left):
+                myList[k] = left[i]
+                i += 1
+                k += 1
+
+            while j < len(right):
+                myList[k]=right[j]
+                j += 1
+                k += 1
+
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.mergeSort(nums)
         return nums
