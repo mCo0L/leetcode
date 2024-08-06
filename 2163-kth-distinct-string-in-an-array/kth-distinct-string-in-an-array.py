caafef2c7041188f0741c42a40f858d7fb388d1b
Mukul Chauhan <12373994+mCo0L@users.noreply.github.com>
2024-08-06 01:03:58 +0100

    Time: 70 ms (58.86%) | Memory: 16.7 MB (75.82%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.py
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.py
@@ -4,11 +4,10 @@ class Solution:
         for ch in arr:
             counter[ch] = counter.get(ch, 0) + 1
         
-        count = 0
         for ch in arr:
             if counter[ch] == 1:
-                count += 1
-                if count == k:
+                k -= 1
+                if k == 0:
                     return ch
         
         return ""
