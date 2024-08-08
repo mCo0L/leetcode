35d6ad17ac28e2afa62b506e6d023576e88aa3ef
Mukul Chauhan <12373994+mCo0L@users.noreply.github.com>
2024-08-08 01:08:57 +0100

    Time: 86 ms (41.02%) | Memory: 17.8 MB (38.67%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.py
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -1,6 +1,36 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        def key(yx, e=1e-9):
-            y, x = yx[0] - rStart, yx[1] - cStart
-            return (max(abs(x), abs(y)), (atan2(y, x) - atan2(-1, 1+e)) % (2 * pi))
-        return sorted(product(range(rows), range(cols)), key=key)
+        ans = []
+        left, right = cStart, cStart+1
+        top, bottom = rStart, rStart+1
+        current = 1
+        move = 0
+        while current <= rows*cols:
+            # fill top
+            for i in range(left+move, right+1):
+                if self.inbound(top, i, rows, cols):
+                    ans.append([top, i])
+                    current += 1
+            left -= 1
+            # fill right
+            for i in range(top+1, bottom+1):
+                if self.inbound(i, right, rows, cols):
+                    ans.append([i, right])
+                    current += 1
+            top -= 1
+            # fill bottom
+            for i in range(right-1, left-1, -1):
+                if self.inbound(bottom, i, rows, cols):
+                    ans.append([bottom, i])
+                    current += 1
+            right += 1
+            # fill left
+            for i in range(bottom-1, top-1, -1):
+                if self.inbound(i, left, rows, cols):
+                    ans.append([i, left])
+                    current += 1
+            bottom += 1
+            move = 1
+        return ans
+    def inbound(self, r, c, rows, cols):
+        return 0<=r<rows and 0<=c<cols
