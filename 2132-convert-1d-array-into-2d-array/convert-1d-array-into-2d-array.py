819171fb9540110e34045385e1c5a39f76057b7b
Mukul Chauhan <12373994+mCo0L@users.noreply.github.com>
2024-09-02 17:42:06 +0100

    Time: 972 ms (5.02%) | Memory: 34.2 MB (9.04%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -0,0 +1,7 @@
+import numpy as np
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if m * n != len(original):
+            return []
+        
+        return np.array(original).reshape((m, n))
